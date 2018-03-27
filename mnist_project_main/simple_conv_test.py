8fcf31cbd6f1b1e3384394b53db4874105e26144
djand <qwasimintah@gmail.com>
2018-03-27 15:37:44 +0200

    Visually wrong classified data

--- mnist_project_main/simple_conv_test.py
+++ mnist_project_main/simple_conv_test.py
@@ -46,7 +46,7 @@ mTrainLabels = np_utils.to_categorical(trainLabels, num_classes)
 mTestLabels = np_utils.to_categorical(testLabels, num_classes)
 
 # grab some test images from the test data
-test_images = testData[10:14]
+test_images = testData
 size = len(test_images)
 org_image = testData[10:14]
 org_image = org_image.reshape(org_image.shape[0],28, 28)
@@ -57,27 +57,58 @@ print("[INFO] test images shape - {}".format(test_images.shape))
 model = load_model('simple_conv')
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 axes =[0]*size
-# loop over each of the test images
-for i, test_image in enumerate(test_images, start=1):
-	# grab a copy of test image for viewing
-	#org_image = test_image
+# # loop over each of the test images
+# for i, test_image in enumerate(test_images, start=1):
+# 	# grab a copy of test image for viewing
+# 	#org_image = test_image
 	
-	# reshape the test image to [1x784] format so that our model understands
-	test_image =test_image.reshape(test_image.shape[0], 1, 28, 28).astype('float32')
+# 	# reshape the test image to [1x784] format so that our model understands
+# 	test_image =test_image.reshape(test_image.shape[0], 1, 28, 28).astype('float32')
 
 	
-	# make prediction on test image using our trained model
-	prediction = model.predict_classes(test_image, verbose=0)
+# 	# make prediction on test image using our trained model
+# 	prediction = model.predict_classes(test_image, verbose=0)
 	
-	# display the prediction and image
-	print("[INFO] I think the digit is - {}".format(prediction[0]))
-	axes[i-1]=plt.subplot(220+i)
+# 	# display the prediction and image
+# 	print("[INFO] I think the digit is - {}".format(prediction[0]))
+# 	axes[i-1]=plt.subplot(220+i)
 
-	plt.imshow(org_image[i-1], cmap=plt.get_cmap('gray'))
+# 	plt.imshow(org_image[i-1], cmap=plt.get_cmap('gray'))
 
-	axes[i-1].text(0.5,-0.1, str(prediction[0]), size=12, ha="center", 
-         transform=axes[i-1].transAxes, color='blue')
+# 	axes[i-1].text(0.5,-0.1, str(prediction[0]), size=12, ha="center", 
+#          transform=axes[i-1].transAxes, color='blue')
 
-	#axes[i-1].set_xlabel(prediction[0])
+# 	#axes[i-1].set_xlabel(prediction[0])
+
+# plt.show()
+
+predicted_classes = model.predict_classes(test_images)
+# see which we predicted correctly and which not
+correct_indices = np.nonzero(predicted_classes == testLabels)[0]
+incorrect_indices = np.nonzero(predicted_classes != testLabels)[0]
+print(len(correct_indices)," classified correctly")
+print(len(incorrect_indices)," classified incorrectly")
+
+# adapt figure size to accomodate 18 subplots
+plt.rcParams['figure.figsize'] = (7,14)
+
+figure_evaluation = plt.figure()
+
+# plot 9 correct predictions
+for i, correct in enumerate(correct_indices[:9]):
+    plt.subplot(6,3,i+1)
+    plt.imshow(test_images[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[correct], testLabels[correct]))
+    plt.xticks([])
+    plt.yticks([])
+
+# plot 9 incorrect predictions
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(6,3,i+10)
+    plt.imshow(test_images[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], testLabels[incorrect]))
+    plt.xticks([])
+    plt.yticks([])
 
 plt.show()
+
