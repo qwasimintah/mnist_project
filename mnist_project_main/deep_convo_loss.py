f7bd77d07587109d7be5bb656c0426ea1b314b67
Antoine <antoine38100@gmail.com>
2018-03-29 00:19:23 +0200

    Adding Kernel 5x5 test results

--- mnist_project_main/deep_convo_loss.py
+++ mnist_project_main/deep_convo_loss.py
@@ -44,17 +44,19 @@ y_train = to_categorical(y_train)
 y_val = to_categorical(y_val)
 #example:
 
+ksize = (7, 7)
+
 model = Sequential()
-model.add(Conv2D(filters = 16, kernel_size = (3, 3), activation='relu',
+model.add(Conv2D(filters = 16, kernel_size = ksize, activation='relu',
                  input_shape = (28, 28, 1)))
 model.add(BatchNormalization())
-model.add(Conv2D(filters = 16, kernel_size = (3, 3), activation='relu'))
+model.add(Conv2D(filters = 16, kernel_size = ksize, activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(strides=(2,2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(filters = 32, kernel_size = (3, 3), activation='relu'))
+model.add(Conv2D(filters = 32, kernel_size = ksize, activation='relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters = 32, kernel_size = (3, 3), activation='relu'))
+model.add(Conv2D(filters = 32, kernel_size = ksize, activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(strides=(2,2)))
 model.add(Dropout(0.25))
@@ -67,9 +69,9 @@ model.add(Dense(10, activation='softmax'))
 
 datagen = ImageDataGenerator(zoom_range = 0.1, height_shift_range = 0.1, width_shift_range= 0.1,rotation_range = 10)
 adam = optimizers.Adam(lr=1e-4)
-#model.compile(loss='categorical_crossentropy', optimizer = rmsprop, metrics=["accuracy"])
+model.compile(loss='categorical_crossentropy', optimizer = adam, metrics=["accuracy"])
 #model.compile(loss='mean_squared_error', optimizer = adam, metrics=["accuracy"])
-model.compile(loss='categorical_hinge', optimizer = adam, metrics=["accuracy"])
+# ~ model.compile(loss='categorical_hinge', optimizer = adam, metrics=["accuracy"])
 
 
 annealer = LearningRateScheduler(lambda x: 1e-3 * 0.9 ** x)
